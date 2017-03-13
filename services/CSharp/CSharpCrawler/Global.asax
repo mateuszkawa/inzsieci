7b1cb38e567ad5ff1b81447c1e36c84c15ef4dcb
Rafał Hejna <rafal.hejna@gmail.com>
2017-03-13 20:57:18 +0100

    Moved to student Azure + renaming project

--- services/CSharp/CSharpCrawler/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSharpCrawler.WebApiApplication" Language="C#" %>
--- services/CSharp/CSharpScraper/App_Start/WebApiConfig.cs
+++ services/CSharp/CSharpScraper/App_Start/WebApiConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Net.Http.Headers;
 using System.Web.Http;
 
-namespace CSharpCrawler
+namespace CSharpScraper
 {
     public static class WebApiConfig
     {
