5f00b7298c27a2c6367916f83a0fa1c8bb434681
rafal.hejna <rafal.hejna@gmail.com>
2017-03-13 13:43:46 +0100

    First draft of C# API

---
+++ services/CSharp/CSharpCrawler/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+
+namespace CSharpCrawler.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [SwaggerOperation("GetAll")]
+        public IEnumerable<string> Get()
+        {
+            return new [] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [SwaggerOperation("GetById")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
---
+++ services/CSharp/CSharpCrawler/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharpCrawler.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
