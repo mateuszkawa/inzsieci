c3db3e08f36d881c81d0aa6d238e7788d7d18574
Rafał Hejna <rafal.hejna@gmail.com>
2017-03-16 23:01:16 +0100

    Fixes for price parsing + Azure Insights

--- services/CSharp/CSharpScraper/Controllers/ProductsController.cs
+++ services/CSharp/CSharpScraper/Controllers/ProductsController.cs
@@ -5,11 +5,16 @@ using HtmlAgilityPack;
 using Swashbuckle.Swagger.Annotations;
 using System.Linq;
 using System;
+using Microsoft.ApplicationInsights;
+using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace CSharpScraper.Controllers
 {
     public class ProductsController : ApiController
     {
+        private TelemetryClient telemetryClient = new TelemetryClient();
+
         // GET api/products/{name}
         [SwaggerOperation("Get")]
         public Product Get(string name)
@@ -27,103 +32,135 @@ namespace CSharpScraper.Controllers
 
         private Product GetMediaMarkt(string name)
         {
-            var result = new List<Product>();
-            var webget = new HtmlWeb();
-            var doc = webget.Load("https://mediamarkt.pl/search?query[querystring]=" + name.Replace(' ', '+'));
-            var products = doc.DocumentNode.SelectNodes("//*[@itemtype='http://schema.org/Product']");
-
-            if (products != null)
+            try
             {
-                foreach (var product in products)
+                var result = new List<Product>();
+                var webget = new HtmlWeb();
+                var doc = webget.Load("https://mediamarkt.pl/search?query[querystring]=" + name.Replace(' ', '+'));
+                var products = doc.DocumentNode.SelectNodes("//*[@itemtype='http://schema.org/Product']");
+
+                if (products != null)
                 {
-                    result.Add(new Product
+                    foreach (var product in products)
                     {
-                        Name = product.SelectSingleNode(".//*[@class='js-product-name']").InnerText.Trim(),
-                        Price = decimal.Parse(product.SelectSingleNode(".//*[@itemprop='price']").Attributes["content"].Value.Replace('.', ',')),
-                        Url = "https://mediamarkt.pl" + product.SelectSingleNode(".//*[@class='js-product-name']").Attributes["href"].Value
-                    });
+                        result.Add(new Product
+                        {
+                            Name = product.SelectSingleNode(".//*[@class='js-product-name']").InnerText.Trim(),
+                            Price = decimal.Parse(Regex.Replace(product.SelectSingleNode(".//*[@itemprop='price']").Attributes["content"].Value.Replace('.', ','), @"\s+", "")),
+                            Url = "https://mediamarkt.pl" + product.SelectSingleNode(".//*[@class='js-product-name']").Attributes["href"].Value
+                        });
+                    }
+                    return result.OrderBy(p => p.Price).ToList()[0];
                 }
-                return result.OrderBy(p => p.Price).ToList()[0];
+            }
+            catch(Exception ex)
+            {
+                telemetryClient.TrackException(ex);
             }
             return new Product() { Name = null, Price = 0.00m, Url = null };
         }
 
         private Product GetSaturn(string name)
         {
-            var result = new List<Product>();
-            var webget = new HtmlWeb();
-            var doc = webget.Load("https://saturn.pl/search?query[querystring]=" + name.Replace(' ', '+'));
-            var products = doc.DocumentNode.SelectNodes("//*[@itemtype='http://schema.org/Product']");
-
-            if (products != null)
+            try
             {
-                foreach (var product in products)
+                var result = new List<Product>();
+                var webget = new HtmlWeb();
+                var doc = webget.Load("https://saturn.pl/search?query[querystring]=" + name.Replace(' ', '+'));
+                var products = doc.DocumentNode.SelectNodes("//*[@itemtype='http://schema.org/Product']");
+
+                if (products != null)
                 {
-                    result.Add(new Product
+                    foreach (var product in products)
                     {
-                        Name = product.SelectSingleNode(".//*[@class='js-product-name']").InnerText.Trim(),
-                        Price = decimal.Parse(product.SelectSingleNode(".//*[@itemprop='price']").Attributes["content"].Value.Replace('.', ',')),
-                        Url = "https://saturn.pl" + product.SelectSingleNode(".//*[@class='js-product-name']").Attributes["href"].Value
-                    });
+                        result.Add(new Product
+                        {
+                            Name = product.SelectSingleNode(".//*[@class='js-product-name']").InnerText.Trim(),
+                            Price = decimal.Parse(Regex.Replace(product.SelectSingleNode(".//*[@itemprop='price']").Attributes["content"].Value.Replace('.', ','), @"\s+", "")),
+                            Url = "https://saturn.pl" + product.SelectSingleNode(".//*[@class='js-product-name']").Attributes["href"].Value
+                        });
+                    }
+                    return result.OrderBy(p => p.Price).ToList()[0];
                 }
-                return result.OrderBy(p => p.Price).ToList()[0];
+            }
+            catch(Exception ex)
+            {
+                telemetryClient.TrackException(ex);
             }
             return new Product() { Name = null, Price = 0.00m, Url = null };
         }
         private Product GetEuro(string name)
         {
-            var result = new List<Product>();
-            var webget = new HtmlWeb();
-            var doc = webget.Load("http://www.euro.com.pl/search.bhtml?keyword=" + name.Replace(' ', '+'));
-            var products = doc.DocumentNode.SelectNodes("//*[@class='product-row']");
-
-            if (products != null)
+            try
             {
-                foreach (var product in products)
+                var result = new List<Product>();
+                var webget = new HtmlWeb();
+                var doc = webget.Load("http://www.euro.com.pl/search.bhtml?keyword=" + name.Replace(' ', '+'));
+                var products = doc.DocumentNode.SelectNodes("//*[@class='product-row']");
+
+                if (products != null)
                 {
-                    var nameNode = product.SelectSingleNode(".//*[@class='product-name']");
-                    result.Add(new Product
+                    foreach (var product in products)
                     {
-                        Name = nameNode.SelectSingleNode(".//*").InnerText.Trim(),
-                        Price = decimal.Parse(product.SelectSingleNode(".//*[@class='price-normal selenium-price-normal']").InnerText.Split(new[] { "&nbsp" }, StringSplitOptions.None)[0].Trim()),
-                        Url = "https://www.euro.com.pl" + nameNode.SelectSingleNode(".//*").Attributes["href"].Value,
-                    });
+                        var nameNode = product.SelectSingleNode(".//*[@class='product-name']");
+                        var price = Regex.Replace(product.SelectSingleNode(".//*[@class='price-normal selenium-price-normal']").InnerText.Split(new[] { "&nbsp" }, StringSplitOptions.None)[0].Trim().Replace(',','.'), @"\s+", "");
+
+                        result.Add(new Product
+                        {
+                            Name = nameNode.SelectSingleNode(".//*").InnerText.Trim(),
+                            Price = decimal.Parse(price, CultureInfo.InvariantCulture),
+                            Url = "https://www.euro.com.pl" + nameNode.SelectSingleNode(".//*").Attributes["href"].Value,
+                        });
+                    }
+                    return result.OrderBy(p => p.Price).ToList()[0];
                 }
-                return result.OrderBy(p => p.Price).ToList()[0];
+            }
+            catch(Exception ex)
+            {
+                telemetryClient.TrackException(ex);
             }
             return new Product() { Name = null, Price = 0.00m, Url = null };
         }
         private Product GetNeonet(string name)
         {
-            var result = new List<Product>();
-            var webget = new HtmlWeb();
-            var doc = webget.Load("https://www.neonet.pl/catalogsearch/result/?q=" + name.Replace(' ', '+'));
-            var products = doc.DocumentNode.SelectNodes("//li");
-
-            if (products != null)
+            try
             {
-                foreach (var product in products)
+                var result = new List<Product>();
+                var webget = new HtmlWeb();
+                var doc = webget.Load("https://www.neonet.pl/catalogsearch/result/?q=" + name.Replace(' ', '+'));
+                var products = doc.DocumentNode.SelectNodes("//li");
+
+                if (products != null)
                 {
-                    var nameNode = product.SelectSingleNode(".//*[@class='product-name']");
-
-                    if (nameNode != null)
+                    foreach (var product in products)
                     {
-                        var priceNode = product.SelectSingleNode(".//*[@class='special-price']");
-                        if (priceNode == null)
-                            priceNode = product.SelectSingleNode(".//*[@class='price']");
-                        else
-                            priceNode = priceNode.SelectSingleNode(".//*[@class='price']");
+                        var nameNode = product.SelectSingleNode(".//*[@class='product-name']");
 
-                        result.Add(new Product
+                        if (nameNode != null)
                         {
-                            Name = nameNode.SelectSingleNode(".//*").InnerText.Trim(),
-                            Price = decimal.Parse(priceNode.InnerText.Split('z')[0].Trim()),
-                            Url = nameNode.SelectSingleNode(".//*").Attributes["href"].Value,
-                        });
+                            var priceNode = product.SelectSingleNode(".//*[@class='special-price']");
+                            if (priceNode == null)
+                                priceNode = product.SelectSingleNode(".//*[@class='price']");
+                            else
+                                priceNode = priceNode.SelectSingleNode(".//*[@class='price']");
+
+                            var price = Regex.Replace(priceNode.InnerText.Split('z')[0].Trim().Replace(',','.'), @"\s+", "");
+
+                            result.Add(new Product
+                            {
+                                Name = nameNode.SelectSingleNode(".//*").InnerText.Trim(),
+                                Price = decimal.Parse(price, CultureInfo.InvariantCulture),
+                                Url = nameNode.SelectSingleNode(".//*").Attributes["href"].Value,
+                            });
+                        }
                     }
+                    if (result.Count > 0)
+                        return result.OrderBy(p => p.Price).ToList()[0];
                 }
-                if(result.Count > 0)
-                    return result.OrderBy(p => p.Price).ToList()[0];
+            }
+            catch(Exception ex)
+            {
+                telemetryClient.TrackException(ex);
             }
             return new Product() { Name = null, Price = 0.00m, Url = null };
         }
